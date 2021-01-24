ea57c14e07e94275252617e3ed7036a32ec647cd
Nataliia Sokolova <newbeadwork@gmail.com>
2021-01-24 14:32:06 -0800

    Adding svg area

---
+++ D3_data_journalism/assets/js/app.js
@@ -0,0 +1,32 @@
+// @TODO: YOUR CODE HERE!
+
+
+//1
+
+var svgArea = d3.select("body").select("svg");
+
+var svgWidth = window.innerWidth;
+var svgHeight = window.innerHeight;
+
+var margin = {
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 50
+};
+
+var height = svgHeight - margin.top - margin.bottom;
+var width = svgWidth - margin.left - margin.right;
+
+
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+//2
+
