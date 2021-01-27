d8245736d35cf3d4a39912700e222643cb13c03e
Nataliia Sokolova <newbeadwork@gmail.com>
2021-01-26 19:23:17 -0800

    First transition

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -9,10 +9,10 @@ var svgWidth = 960;
 var svgHeight = 500;
 
 var margin = {
-  top: 50,
-  right: 50,
-  bottom: 50,
-  left: 50
+  top: 80,
+  right: 80,
+  bottom: 80,
+  left: 80
 };
 
 var height = svgHeight - margin.top - margin.bottom;
@@ -29,38 +29,40 @@ var chartGroup = svg.append("g")
 
 
 //2
-d3.csv("assets/data/data.csv").then(function(healthData) {
-    console.log(healthData);
+d3.csv("assets/data/data.csv").then(function (healthData) {
+  console.log(healthData);
 
-    // parsing the data
-  healthData.forEach(function(data) {
+  // parsing the data
+  healthData.forEach(function (data) {
     data.poverty = +data.poverty;
     data.healthcare = +data.healthcare;
   });
 
   // creating scales
+  
   var xPovertyScale = d3.scaleLinear()
-    .domain(d3.extent(healthData, d => d.poverty))
-    .range([0, width]);
+  .domain(d3.extent(healthData, d => d.poverty))
+  .range([0, width]);
 
   var yHealthcareScale = d3.scaleLinear()
     .domain([0, d3.max(healthData, d => d.healthcare)])
     .range([height, 0]);
-  
-    // creating axes
+
+  // creating axes
   var xAxis = d3.axisBottom(xPovertyScale);
-  var yAxis = d3.axisLeft(yHealthcareScale).ticks(10);
+  var yAxis = d3.axisLeft(yHealthcareScale);
 
   // appending axes
   chartGroup.append("g")
-    .attr("transform", `translate(0, ${height - 40})`)
+    .attr("transform", `translate(0, ${height})`)
+    .attr("class", "x_axis")
     .call(xAxis);
 
   chartGroup.append("g")
-  .attr("transform", `translate(0, -40)`)
+    .attr("class", "y_axis")
     .call(yAxis);
 
-  
+//unction getData(healthdata)
 
   // appending circles
   var circlesGroup = chartGroup.selectAll("circle")
@@ -69,7 +71,7 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
     .append("circle")
     .attr("cx", d => xPovertyScale(d.poverty))
     .attr("cy", d => yHealthcareScale(d.healthcare))
-    .attr("r", "10")
+    .attr("r", "12")
     .attr("fill", "green")
     .attr("stroke-width", "1")
     .attr("stroke", "black");
@@ -88,57 +90,90 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
     .attr("fill", "gold");
 
   var xLabelPoverty = chartGroup.append("text")
-    .attr("transform", `translate(${width / 2}, ${height + margin.top - 55})`)
+    .attr("transform", `translate(${width / 2}, ${height + 30})`)
     .attr("text-anchor", "middle")
     .attr("font-size", "16px")
     .attr("fill", "green")
     .text("In Poverty (%)");
 
   var xLabelAge = chartGroup.append("text")
-  .attr("transform", `translate(${width / 2}, ${height + margin.top - 35})`)
-  .attr("text-anchor", "middle")
-  .attr("font-size", "16px")
-  .attr("fill", "green")
-  .attr("opacity", 0.3)
-  .text("Age (Median)");
+    .attr("transform", `translate(${width / 2}, ${height + 50})`)
+    .attr("text-anchor", "middle")
+    .attr("font-size", "16px")
+    .attr("fill", "green")
+    .attr("opacity", 0.3)
+    .text("Age (Median)");
 
-  var xLabelAge = chartGroup.append("text")
-  .attr("transform", `translate(${width / 2}, ${height + margin.top - 15})`)
-  .attr("text-anchor", "middle")
-  .attr("font-size", "16px")
-  .attr("fill", "green")
-  .attr("opacity", 0.3)
-  .text("Household Income (Median)");
-
-
+  var xLabelIncome = chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + 70})`)
+    .attr("text-anchor", "middle")
+    .attr("font-size", "16px")
+    .attr("fill", "green")
+    .attr("opacity", 0.3)
+    .text("Household Income (Median)");
 
   // y-axis title
-  var yLabel = chartGroup.append("text")
-    // this rotation makes things weird!
-    // x and y placements will seem transposed.
+  var yLabelHealthcare = chartGroup.append("text")
     .attr("transform", "rotate(-90)")
     .attr("x", 0 - height / 2)
-    .attr("y", 0 - margin.left)
-    // "em" used to offset the text. 1em is equivalent to 
-    // the font size. For example, if default text is 16px, then 1 em is 16, 
-    // 2 is 32px, etc. Used to dynamically place text regardless of size.
+    .attr("y", 0 - 40)
     .attr("dy", "1em")
     .style("text-anchor", "middle")
     .attr("fill", "green")
-    .text("Lacks Healthcare (%)"); 
+    .text("Lacks Healthcare (%)");
 
-  barsGroup.on("mouseover", function () {
-    d3.select(this)
+  var yLabelHealthSmoke = chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("x", 0 - height / 2)
+    .attr("y", 0 - 60)
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .attr("fill", "green")
+    .attr("opacity", 0.3)
+    .text("Smokes (%)");
+
+  var yLabelHealthcare = chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("x", 0 - height / 2)
+    .attr("y", 0 - margin.left)
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .attr("fill", "green")
+    .attr("opacity", 0.3)
+    .text("Obese (%)");
+
+  xLabelAge.on("mouseover", function () {
+
+
+
+    var xAgeScale = d3.scaleLinear()
+      .domain(d3.extent(healthData, d => d.age))
+      .range([0, width]);
+    xAxis = d3.axisBottom(xAgeScale);
+    d3.selectAll(".x_axis")
       .transition()
-      .duration(500)
-      .attr("fill", "red");
-  })
-    .on("mouseout", function () {
+      .duration(3000)
+      .call(xAxis);
+
+    circlesGroup
+      .transition()
+      .attr("cx", d => xAgeScale(d.age));
+
+    circlesLabels
+      .transition()
+      .text(d => d.abbr)
+      .attr("x", d => xAgeScale(d.age));
+      
+
+
+  });
+
+  /*.on("mouseout", function () {
       d3.select(this)
         .transition()
         .duration(500)
         .attr("fill", "green");
-    });
+    });*/
 
 }
   , function (error) {
